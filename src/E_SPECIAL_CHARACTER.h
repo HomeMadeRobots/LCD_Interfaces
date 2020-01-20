86bb49013bbc200718014e33a0f6fb2a61a277fe
HomeMadeRobots <59938642+HomeMadeRobots@users.noreply.github.com>
2020-01-20 10:08:05 +0100

    Add files via upload

---
+++ src/E_SPECIAL_CHARACTER.h
@@ -0,0 +1,12 @@
+#ifndef E_SPECIAL_CHARACTER_H
+#define E_SPECIAL_CHARACTER_H
+
+/* Enumerated_Data_Type */
+/* Allows to model a data representing a special character that can be print on a LCD. */
+typedef enum E_SPECIAL_CHARACTER {
+    SUN_CHARACTER,
+    MOON_CHARACTER,
+    SMILEY_CHARACTER
+} E_SPECIAL_CHARACTER;
+
+#endif
